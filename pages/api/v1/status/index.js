392e1a79d3795b44b33c32b0254d5f8bb0368c9f
brunoblda <brunoblda@gmail.com>
2025-04-24 19:59:56 -0300

    :sparkles: feat: add `/api/vi/status` endpoint

---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,15 @@
+function status(request, response) {
+  const { method } = request;
+
+  if (method === 'GET') {
+    response.status(200).json({ status: 'ok' });
+  } else {
+    response.setHeader('Allow', ['GET']);
+    response.status(405).end(`Method ${method} Not Allowed`);
+  }
+}
+export default status;
+
+// This API route handles GET requests to check the status of the server.
+// If the request method is not GET, it responds with a 405 Method Not Allowed status.
+// The response also includes an Allow header indicating that only GET requests are allowed.
