27933c8c6aa6c2f570c24a88e930e0aebfdf3c1e
brunoblda <brunoblda@gmail.com>
2026-09-03 12:14:34 -0300

    style: fixing formatting

--- tests/integration/api/vi/status/get.test.js
+++ tests/integration/api/vi/status/get.test.js
@@ -5,25 +5,20 @@ beforeAll(async () => {
 });
 
 describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch(`http://localhost:3000/api/v1/status`);
+      expect(response.status).toBe(200);
 
-    describe("Anonymous user", () => {
+      const responseBody = await response.json();
 
-        test("Retrieving current system status", async () => {
-        const response = await fetch(`http://localhost:3000/api/v1/status`);
-        expect(response.status).toBe(200);
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-        const responseBody = await response.json();
-
-        const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
-        expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
-
-        expect(responseBody.status).toEqual("ok");
-        expect(responseBody.dependencies.database.version).toEqual("16.8");
-        expect(responseBody.dependencies.database.max_connections).toEqual(100);
-        expect(responseBody.dependencies.database.opened_connections).toEqual(1);
-        });
-
+      expect(responseBody.status).toEqual("ok");
+      expect(responseBody.dependencies.database.version).toEqual("16.8");
+      expect(responseBody.dependencies.database.max_connections).toEqual(100);
+      expect(responseBody.dependencies.database.opened_connections).toEqual(1);
     });
-
+  });
 });
-
